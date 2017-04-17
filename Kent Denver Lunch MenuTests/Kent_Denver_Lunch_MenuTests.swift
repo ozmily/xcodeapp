a748f4a3ad0e683693ab902fe6d3714222400d92
Osamah Zmily <ozmily17@kentdenver.org>
2017-04-17 14:31:20 -0600

    Initial Commit

---
+++ Kent Denver Lunch MenuTests/Kent_Denver_Lunch_MenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kent_Denver_Lunch_MenuTests.swift
+//  Kent Denver Lunch MenuTests
+//
+//  Created by Osamah Zmily on 17/04/2017.
+//  Copyright © 2017 Kent Denver Lunch. All rights reserved.
+//
+
+import XCTest
+@testable import Kent_Denver_Lunch_Menu
+
+class Kent_Denver_Lunch_MenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
